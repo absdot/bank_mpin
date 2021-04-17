ceacd0110e8af692861faf35aeaaf86ab85c935d
Afzal Ali <afzalali15@gmail.com>
2021-04-17 19:26:20 +0400

    added text animation

--- lib/mpin/mpin_anim.dart
+++ lib/mpin/mpin_anim.dart
@@ -9,6 +9,7 @@ class _MPinAnimState extends State<MPinAnim>
     with SingleTickerProviderStateMixin {
   AnimationController _controller;
   Animation<double> _sizeAnimation;
+  Animation<double> _opacityAnimation;
 
   @override
   void initState() {
@@ -22,6 +23,7 @@ class _MPinAnimState extends State<MPinAnim>
       });
 
     _sizeAnimation = Tween<double>(begin: 24, end: 72).animate(_controller);
+    _opacityAnimation = Tween<double>(begin: 0, end: 1).animate(_controller);
     _controller.forward();
     super.initState();
   }
@@ -31,9 +33,23 @@ class _MPinAnimState extends State<MPinAnim>
     return Container(
       height: _sizeAnimation.value,
       width: _sizeAnimation.value,
+      alignment: Alignment.center,
       decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(_sizeAnimation.value / 2),
           color: Colors.white),
+      child: Opacity(
+        opacity: _opacityAnimation.value,
+        child: Transform.scale(
+          scale: _sizeAnimation.value / 48,
+          child: Text(
+            '1',
+            style: Theme.of(context)
+                .textTheme
+                .headline6
+                .copyWith(fontWeight: FontWeight.bold),
+          ),
+        ),
+      ),
     );
   }
 }
